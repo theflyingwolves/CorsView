e80ca5afde07f684b7bb17d2d72cc4e46354ec4e
Wang Yichao <wangyichao@r-49-177-28-172.comp.nus.edu.sg>
2014-08-27 14:39:47 +0800

    make some change to the transition after clicking module book

--- src/js/animations/course-animate.js
+++ src/js/animations/course-animate.js
@@ -6,16 +6,38 @@ $('.modulebook').mouseenter(function(){
 	$(this).css("opacity","1.0");
 });
 
-$('mousebook').click(function(){
-	$(this).animate({
-		width:'100%'
+$('.modulebook').click(function(){
+	var offsetLeft = $(this).offset().left;
+	//color the background with the module book color and make the color deeper
+	var currentColor = $(this).css("background-color");
+    var lastComma = currentColor.lastIndexOf(')');
+    var newColor = "rgba"+currentColor.slice(3, lastComma) + ", 0.3 )";
+	$(".moduleshelf").css("background-color", newColor);
+	$(this).prevAll().animate({
+		left: -offsetLeft
 	},300);
+	$(this).find("h1").css("-ms-writing-mode", "tb-rl");
+	$(this).find("h1").css("-webkit-writing-mode","vertical-rl"); 
+	$(this).find("h1").css("-moz-writing-mode","vertical-rl");
+	$(this).find("h1").css("-ms-writing-mode","vertical-rl");
+	$(this).find("h1").css("writing-mode","vertical-lr");
+	$(this).find("p").animate({
+		opacity:0
+	},150)
+	$(this).animate({
+		width:'20px',
+		left: -offsetLeft
+	},300);
+	$(this).nextAll().animate({
+		right:-$(window).width()-offsetLeft
+	})
+	console.log(newColor+" "+currentColor);
+
 })
 
 
 $('.moduleshelf').mouseout(function(){
 	$('.modulebook').css("opacity","1.0");
-	console.log("out");
 });
 
 $('#fb-icon').mouseover(function(){
