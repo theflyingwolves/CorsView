9d80ee6a5282639dd7b7b967543701f6c5b0ad4b
wulifu2hao <wulifu20hao@gmail.com>
2014-09-05 21:25:58 +0800

    fix minor bugs in api

--- api/users_modules.php
+++ api/users_modules.php
@@ -5,11 +5,12 @@ require_once('common_functions.php');
 
 function getModulesTaken($userID){
     if($userID != null){
-         $mysqli = connect_database();
+        $mysqli = connect_database();
 
         $newQuery = sprintf("SELECT * FROM " . MODULES_TABLE . ",". ENROLLMENTS_TABLE . " WHERE " . MODULES_TABLE . ".Module_ID =" . ENROLLMENTS_TABLE .
             ".Module_ID AND User_ID = '%s' AND " . MODULES_TABLE . ".Deleted = 0 AND " . ENROLLMENTS_TABLE . ".Deleted = 0",
-                $mysqli->real_escape_string($userID));
+            $mysqli->real_escape_string($userID));
+        
         if($result = $mysqli->query($newQuery)){
             $moduleList = array();
             while($row = $result->fetch_array(MYSQLI_ASSOC)){
@@ -31,62 +32,62 @@ function getModulesTaken($userID){
     }
 }
 
-function addEnrollmentList($enrollmentsDetailList){
-    if ($enrollmentsDetailList != null) {
-         $mysqli = connect_database();
+// function addEnrollmentList($enrollmentsDetailList){
+//     if ($enrollmentsDetailList != null) {
+//          $mysqli = connect_database();
         
-        if(!authentication($enrollmentsDetailList['userID'],$enrollmentsDetailList['accessToken'])){
-            $returnMessage = "user is not authorized.";
-            respondToClient(403,array('message' => $returnMessage));
-            return;
-        }
+//         if(!authentication($enrollmentsDetailList['userID'],$enrollmentsDetailList['accessToken'])){
+//             $returnMessage = "user is not authorized.";
+//             respondToClient(403,array('message' => $returnMessage));
+//             return;
+//         }
 
-        $count = 0;
+//         $count = 0;
 
-        for ($enrollmentsDetailList['list'] as $enrollmentsDetails){
+//         foreach ($enrollmentsDetailList['list'] as $enrollmentsDetails){
 
-            // check whether the module code exist in the db
-            $newQuery = sprintf("SELECT * FROM " . MODULES_TABLE ." WHERE Module_Code = '%s' AND Deleted = 0",
-                $mysqli->real_escape_string($enrollmentsDetails['moduleCode']));
-            $result=$mysqli->query($newQuery);
-            // create enrollment only when the module code exist in the db
-            // deal with this issue later
-            if($row = $result->fetch_array(MYSQLI_ASSOC)){
-                $moduleID = $row['Module_ID'];
+//             // check whether the module code exist in the db
+//             $newQuery = sprintf("SELECT * FROM " . MODULES_TABLE ." WHERE Module_Code = '%s' AND Deleted = 0",
+//                 $mysqli->real_escape_string($enrollmentsDetails['moduleCode']));
+//             $result=$mysqli->query($newQuery);
+//             // create enrollment only when the module code exist in the db
+//             // deal with this issue later
+//             if($row = $result->fetch_array(MYSQLI_ASSOC)){
+//                 $moduleID = $row['Module_ID'];
 
-                $newQuery = sprintf("SELECT * FROM " . ENROLLMENTS_TABLE ." WHERE Module_ID = '%s' AND User_ID = '%s' AND Deleted = 0",
-                    $mysqli->real_escape_string($moduleID),
-                    $mysqli->real_escape_string($enrollmentsDetails['userID']));
-                $mysqli->query($newQuery);
-                if($mysqli->affected_rows != 0){
-                    break;
-                } else{
-                    $newQuery = sprintf("INSERT INTO ". ENROLLMENTS_TABLE ." (Module_ID,User_ID,Created_Time) VALUES ('%s','%s','%s')",
-                            $mysqli->real_escape_string($moduleID),
-                            $mysqli->real_escape_string($enrollmentsDetails['userID']),
-                            $mysqli->real_escape_string(timeGenerator()));
-                    // note: remember to handle exception here, e.g. inserting the same enrollment
-                $mysqli->query($newQuery);
-                if($mysqli->affected_rows != 0){
-                        $count += 1;
-                    }
-                }
+//                 $newQuery = sprintf("SELECT * FROM " . ENROLLMENTS_TABLE ." WHERE Module_ID = '%s' AND User_ID = '%s' AND Deleted = 0",
+//                     $mysqli->real_escape_string($moduleID),
+//                     $mysqli->real_escape_string($enrollmentsDetails['userID']));
+//                 $mysqli->query($newQuery);
+//                 if($mysqli->affected_rows != 0){
+//                     break;
+//                 } else{
+//                     $newQuery = sprintf("INSERT INTO ". ENROLLMENTS_TABLE ." (Module_ID,User_ID,Created_Time) VALUES ('%s','%s','%s')",
+//                             $mysqli->real_escape_string($moduleID),
+//                             $mysqli->real_escape_string($enrollmentsDetails['userID']),
+//                             $mysqli->real_escape_string(timeGenerator()));
+//                     // note: remember to handle exception here, e.g. inserting the same enrollment
+//                 $mysqli->query($newQuery);
+//                 if($mysqli->affected_rows != 0){
+//                         $count += 1;
+//                     }
+//                 }
 
-            } 
+//             } 
 
             
-        }
+//         }
 
-        $mysqli->close();
+//         $mysqli->close();
 
-        $returnMessage = strval($count) . " modules enrolled successfully";
-        respondToClient(200,array('message' => $returnMessage));        
+//         $returnMessage = strval($count) . " modules enrolled successfully";
+//         respondToClient(200,array('message' => $returnMessage));        
 
-    } else{
-            $returnMessage = "enrollments detail is missing";
-            respondToClient(400,array('message' => $returnMessage));
-    }
-}
+//     } else{
+//             $returnMessage = "enrollments detail is missing";
+//             respondToClient(400,array('message' => $returnMessage));
+//     }
+// }
 
 function addEnrollment($enrollmentsDetails){
     if ($enrollmentsDetails != null) {
